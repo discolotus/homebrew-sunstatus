79a94d76ba9e7f4295e2575d4b4882b8db425f80
Tanner Leo <tannerleo@MacBook-Air-88.local>
2026-06-08 08:42:43 -0700

    Update SunStatus cask to 0.2.3

--- Casks/sunstatus.rb
+++ Casks/sunstatus.rb
@@ -1,6 +1,6 @@
 cask "sunstatus" do
-  version "0.2.2"
-  sha256 "17be01b86510b798e1fe6480eac5581f1a7a6bd0be843ec10e6d236304a38f49"
+  version "0.2.3"
+  sha256 "aa481efc08c8750159992157dd5dc556e6869898883739de71f9b6ec287b3e06"
 
   url "https://github.com/discolotus/SunStatus/releases/download/v#{version}/SunStatus.zip"
   name "SunStatus"
