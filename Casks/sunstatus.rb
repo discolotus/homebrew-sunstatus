94c9df21a574e8d06068a06c06c8ee8a6d530d84
Tanner Leo <tannerleo@MacBook-Air-88.local>
2026-06-07 22:08:05 -0700

    Add SunStatus cask

---
+++ Casks/sunstatus.rb
@@ -0,0 +1,20 @@
+cask "sunstatus" do
+  version "0.1.0"
+  sha256 "ee385e2fb844e10b45fbf4a870aef2895154f9105f50dc959cb8ebc7c9090785"
+
+  url "https://github.com/discolotus/SunStatus/releases/download/v#{version}/SunStatus.zip",
+      verified: "github.com/discolotus/SunStatus/"
+  name "SunStatus"
+  desc "Menu bar app for daylight status at a glance"
+  homepage "https://github.com/discolotus/SunStatus"
+
+  depends_on macos: ">= :ventura"
+
+  app "SunStatus.app"
+
+  zap trash: [
+    "~/Library/Application Support/SunStatus",
+    "~/Library/Caches/com.discolotus.SunStatus",
+    "~/Library/Preferences/com.discolotus.SunStatus.plist",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,22 @@
+# SunStatus Homebrew Tap
+
+Install SunStatus with Homebrew:
+
+```sh
+brew install --cask discolotus/sunstatus/sunstatus
+```
+
+Upgrade after new releases:
+
+```sh
+brew update
+brew upgrade --cask sunstatus
+```
+
+Uninstall:
+
+```sh
+brew uninstall --cask sunstatus
+```
+
+SunStatus source and releases live at https://github.com/discolotus/SunStatus.
